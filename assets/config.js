2514a3dbbeaaa7e3f04952cb03254217e7355621
wadeteam <116362097+wadeteam@users.noreply.github.com>
2023-01-22 01:00:00 +0100

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 const config = {
     
-    USERNAME: "User#0000",
+    USERNAME: "dejsirizek#6223",
     /**
      * This can either be a file link (relative or external)
      * or a Gravatar email or hash.
@@ -15,9 +15,10 @@ const config = {
     ],
 
     PROJECTS: [
-        { id: "project-1", image: "assets/img/placeholder1.png", link: "https://discord.com", name: "Super cool project 1" },
-        { id: "project-2", image: "assets/img/placeholder2.png", link: "https://discord.com", name: "Super cool project 2" },
-        { id: "project-3", image: "assets/img/placeholder3.png", link: "https://discord.com", name: "Super cool project 3" },
+        { id: "project-1", image: "assets/img/placeholder1.png", link: "https://chill-games.eu", name: "Chill-Games" },
+        { id: "project-2", image: "assets/img/placeholder2.png", link: "https://moon-bot.eu", name: "MOON Bot" },
+        { id: "project-3", image: "assets/img/placeholder3.png", link: "https://blaze-staking.eu", name: "Blaze-Staking" },
+        { id: "project-4", image: "assets/img/placeholder3.png", link: "http://wadeplayer.xyz", name: "WADE Player" },
     ],
 
     SOCIALS: [
